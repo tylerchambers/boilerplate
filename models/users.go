c0d9077a4f9151550db34484cf1ed8143b48a7ce
Tyler Chambers <me@tylerchambers.net>
2021-08-04 21:31:31 -0400

    add user models

---
+++ models/users.go
@@ -0,0 +1,96 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents a user of our service.
+type User struct {
+	Username     string
+	Email        string
+	Password     string
+	PasswordSet  time.Time
+	Registered   time.Time
+	Premium      bool
+	PremiumSince time.Time
+	PremiumUntil time.Time
+	Subscribed   bool
+	SubscribedOn time.Time
+}
+
+func (u *User) SetUsername(username string) {
+	// TODO: check if username is already taken, etc.
+	u.Username = username
+}
+
+func (u *User) SetEmail(email string) {
+	// TODO: check if email is already taken, do validation.
+	u.Email = email
+}
+
+// SetPassword hashes, then set's a user's password.
+func (u *User) SetPassword(now time.Time, clearPw string) error {
+	if len(clearPw) <= 8 {
+		return errors.New("password must be 8 characters or longer")
+	}
+	bytes, err := bcrypt.GenerateFromPassword([]byte(clearPw), 14)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bytes)
+	u.PasswordSet = now
+	return nil
+}
+
+// CheckPassword verifies a cleartext password against a user's password.
+func (u *User) CheckPassword(clearPw string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(clearPw))
+	return err == nil
+}
+
+// MakePremium marks a user's account as premium.
+func (u *User) MakePremium(now time.Time, until time.Time) error {
+	if u.Premium {
+		return errors.New("already premium")
+	}
+	u.Premium = true
+	if u.PremiumSince.IsZero() {
+		u.PremiumSince = now
+	}
+	u.PremiumUntil = until
+	return nil
+}
+
+// MakeNonPremium demotes a user from premium / paid down to a regular user.
+func (u *User) MakeNonPremium() {
+	u.Premium = false
+	// Set PremiumSince back to 0
+	u.PremiumSince = time.Time{}
+}
+
+// Subscribe marks a user as "subscribed", used for recurring billing.
+func (u *User) Subscribe(now time.Time) {
+	u.Subscribed = true
+	u.SubscribedOn = now
+}
+
+// Unsubscribe unsusbscribes a user from our service.
+func (u *User) Unsubscribe() {
+	u.Subscribed = false
+}
+
+// NewUser sets up a new user account.
+func NewUser(now time.Time, username, email, password string) (*User, error) {
+	u := new(User)
+	u.SetUsername(username)
+	u.SetEmail(email)
+	err := u.SetPassword(now, password)
+	if err != nil {
+		return nil, errors.New("could not register new user")
+	}
+	u.Registered = now
+	return u, nil
+}
