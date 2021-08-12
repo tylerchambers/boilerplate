82a6cc436bcfec864089d245938081a348d21ae9
Tyler Chambers <me@tylerchambers.net>
2021-08-11 22:54:34 -0400

    add status handler

--- app/handlers.go
+++ app/handlers.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-func (s *Server) apiRootHandler() http.HandlerFunc {
+func (s *Server) statusHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var response map[string]string
-		json.Unmarshal([]byte(`{ "hello": "world" }`), &response)
+		json.Unmarshal([]byte(`{ "status": "OK" }`), &response)
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
-		w.WriteHeader(http.StatusOK)
 	}
 }
